e16e1f248ec0964875d6b268bff02382a3b94736
Okky Julian Atmajaya Tarmoko <oatmajaya@student.ciputra.ac.id>
2024-07-31 10:34:44 +0700

    Form Paket Bakteri Done

--- Waterpediaa/FormPaketBakteri.cs
+++ Waterpediaa/FormPaketBakteri.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,191 @@ namespace Waterpediaa
 {
     public partial class FormPaketBakteri : Form
     {
+        private DataTable tempTable;
         public FormPaketBakteri()
         {
             InitializeComponent();
+            this.Load += new EventHandler(FormPaketBakteri_Load);
+        }
+
+        private void FormPaketBakteri_Load(object sender, EventArgs e)
+        {
+            LoadPaketData();
+            LoadBakteriNames();
+            LoadPackagingNames();
+            LoadPilihPaket();
+            InitializeTempTable();
+        }
+
+        private void LoadPaketData()
+        {
+            string connectionString = "server=localhost;database=waterpedia;user=root;";
+            string query = @"SELECT pb.Nama_Paket, sb.Jenis_Bakteri, sp.Nama_Barang AS 'Jenis Packaging'
+                             FROM Paket_Bakteri pb
+                             JOIN Stock_Bakteri sb ON pb.Stock_BakteriID = sb.ID
+                             JOIN Stock_Packaging sp ON pb.Stock_PackagingID = sp.ID";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                {
+                    connection.Open();
+                    DataTable dataTable = new DataTable();
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(cmd))
+                    {
+                        adapter.Fill(dataTable);
+                    }
+                    dataGridViewPaket.DataSource = dataTable;
+
+                    // Ensure Nama_Paket is the first column
+                    dataGridViewPaket.Columns["Nama_Paket"].DisplayIndex = 0;
+
+                    // Optionally, hide ID column if it's part of the query
+                    // dataGridViewPaket.Columns["ID"].Visible = false;
+                }
+            }
+        }
+
+        private void LoadBakteriNames()
+        {
+            string connectionString = "server=localhost;database=waterpedia;user=root;";
+            string query = "SELECT ID, Jenis_Bakteri FROM Stock_Bakteri";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                {
+                    connection.Open();
+                    DataTable dataTable = new DataTable();
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(cmd))
+                    {
+                        adapter.Fill(dataTable);
+                    }
+                    comboBoxBakteri.DataSource = dataTable;
+                    comboBoxBakteri.DisplayMember = "Jenis_Bakteri";
+                    comboBoxBakteri.ValueMember = "ID";
+                }
+            }
+        }
+
+        private void LoadPackagingNames()
+        {
+            string connectionString = "server=localhost;database=waterpedia;user=root;";
+            string query = "SELECT ID, Nama_Barang FROM Stock_Packaging";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                {
+                    connection.Open();
+                    DataTable dataTable = new DataTable();
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(cmd))
+                    {
+                        adapter.Fill(dataTable);
+                    }
+                    comboBoxPackaging.DataSource = dataTable;
+                    comboBoxPackaging.DisplayMember = "Nama_Barang";
+                    comboBoxPackaging.ValueMember = "ID";
+                }
+            }
+        }
+
+        private void LoadPilihPaket()
+        {
+            string connectionString = "server=localhost;database=waterpedia;user=root;";
+            string query = "SELECT ID, Nama_Paket FROM Paket_Bakteri";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                {
+                    connection.Open();
+                    DataTable dataTable = new DataTable();
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(cmd))
+                    {
+                        adapter.Fill(dataTable);
+                    }
+                    comboBoxPilihPaket.DataSource = dataTable;
+                    comboBoxPilihPaket.DisplayMember = "Nama_Paket";
+                    comboBoxPilihPaket.ValueMember = "ID";
+                }
+            }
+        }
+
+        private void InitializeTempTable()
+        {
+            tempTable = new DataTable();
+            tempTable.Columns.Add("Stock_BakteriID", typeof(int));
+            tempTable.Columns.Add("Stock_PackagingID", typeof(int));
+            tempTable.Columns.Add("Nama_Paket", typeof(string));
+
+            dataGridViewTemp.DataSource = tempTable;
+        }
+
+        private void buttonBuat_Click(object sender, EventArgs e)
+        {
+            string namaPaket = textBoxNamaPaket.Text;
+
+            string connectionString = "server=localhost;database=waterpedia;user=root;";
+            string query = "INSERT INTO Paket_Bakteri (Nama_Paket) VALUES (@namaPaket)";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@namaPaket", namaPaket);
+
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            LoadPaketData();
+            LoadPilihPaket(); // Load the new package into comboBoxPilihPaket
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            DataRowView selectedPaket = comboBoxPilihPaket.SelectedItem as DataRowView;
+            DataRowView selectedBakteri = comboBoxBakteri.SelectedItem as DataRowView;
+            DataRowView selectedPackaging = comboBoxPackaging.SelectedItem as DataRowView;
+
+            if (selectedPaket != null && selectedBakteri != null && selectedPackaging != null)
+            {
+                DataRow newRow = tempTable.NewRow();
+                newRow["Stock_BakteriID"] = selectedBakteri["ID"];
+                newRow["Stock_PackagingID"] = selectedPackaging["ID"];
+                newRow["Nama_Paket"] = selectedPaket["Nama_Paket"];
+
+                tempTable.Rows.Add(newRow);
+            }
+        }
+
+        private void buttonNewPaket_Click(object sender, EventArgs e)
+        {
+            string connectionString = "server=localhost;database=waterpedia;user=root;";
+            string query = "INSERT INTO Paket_Bakteri (Stock_BakteriID, Stock_PackagingID, Nama_Paket) " +
+                           "VALUES (@stockBakteriID, @stockPackagingID, @namaPaket)";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                foreach (DataRow row in tempTable.Rows)
+                {
+                    using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@stockBakteriID", row["Stock_BakteriID"]);
+                        cmd.Parameters.AddWithValue("@stockPackagingID", row["Stock_PackagingID"]);
+                        cmd.Parameters.AddWithValue("@namaPaket", row["Nama_Paket"]);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+
+            LoadPaketData();
+            tempTable.Clear();
         }
     }
 }
--- Waterpediaa/obj/Debug/net6.0-windows/Waterpediaa.AssemblyInfo.cs
+++ Waterpediaa/obj/Debug/net6.0-windows/Waterpediaa.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Waterpediaa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+684aa98a3747ffc9ce826b557c01414f484b22dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e792dac57acb1a0e5851d50410ced8131a7327e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Waterpediaa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Waterpediaa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
